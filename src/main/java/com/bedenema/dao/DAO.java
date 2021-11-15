db4b8c09a95f14e093d551a879547eeb1068de13
dung0312-ptit <dungtn312@gmail.com>
2021-11-15 16:25:25 +0700

    add DAO

--- src/main/java/com/bedenema/dao/DAO.java
+++ src/main/java/com/bedenema/dao/DAO.java
@@ -1,7 +1,16 @@
 package com.bedenema.dao;
 
-import java.sql.Connection;
+import java.sql.*;
 
-public class DAO {
+public class DAO<conn> {
+
+    private String jdbcUrl = "jdbc:mariadb://localhost:3306/bedenema_db";
+    private    String username = "root";
+    private    String password ="";
+    private Connection conn;
+
+    public DAO() throws SQLException {
+        conn = DriverManager.getConnection(jdbcUrl,username,password);
+    }
 
 }
